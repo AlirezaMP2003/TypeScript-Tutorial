e1f55e504edf53762d632bc9a20b4eaf774e340a
Alireza Mousapour <alirezamoosapour7777@gmail.com>
2024-12-15 18:31:43 +0330

    Control structures

---
+++ src/3/3-3.ts
@@ -0,0 +1,84 @@
+// Conditional Statements:
+
+// 1_ if else:
+
+// if (condition) {
+    // code
+// } esle if (condition) {
+    // code
+// } else {
+    // code
+// }
+
+
+// 2_ switch:
+
+// switch (value) {
+    // case value1:
+        // code
+        // break; // flow control
+    // case value2:
+        // code
+        // break; // flow control
+    // default:
+        // code
+// }
+
+
+// 3_ ternary operator:
+let age3 = 20;
+let category = age3 >= 18 ? "Adult" : "Minor";
+console.log(category); // "Adult"
+
+
+
+// Loops:
+
+// 1_ for:
+
+// for (start; condition; change) {
+//     code
+// }
+
+
+// 2_ while:
+
+// while (condition) {
+//     code
+// }
+
+
+// 3_ do-while:
+
+// do{
+//     code
+// } while(condition)
+
+
+// 4_ for-of:
+
+// for(let element of array) {
+//     code
+// }
+
+
+// 5_ for-in:
+
+// for(let key in object) {
+//     code
+// }
+
+
+// Flow Control:
+
+// 1_ break
+// 2_ continue
+// 3_ return
+// 4_ throw
+// 5_ try-catch-finally
+
+
+
+
+
+// ts-node src/3/3-3.ts
