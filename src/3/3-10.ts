6dc905ea65c46912bbc918fbb90aa64be9c96c28
Alireza Mousapour <alirezamoosapour7777@gmail.com>
2024-12-16 10:30:41 +0330

    Overloading

---
+++ src/3/3-10.ts
@@ -0,0 +1,40 @@
+// Overloading
+
+
+function display(value: string): void;
+function display(value: number): void;
+
+function display(value: string | number): void {
+    if (typeof value === "string") {
+        console.log("String value: " + value);
+    } else {
+        console.log("Number value: " + value);
+    }
+}
+
+display("Hello"); // String value: Hello
+display(13);      // Number value: 13
+
+
+
+
+function sum(a: number, b: number): number;
+function sum(a: number, b: number, c: number): number;
+
+function sum(a: number, b: number, c?: number): number {
+    if (c !== undefined) {
+        return a + b + c;
+    }
+    return a + b;
+}
+
+console.log(sum(1, 2));       // 3
+console.log(sum(1, 2, 3));    // 6
+
+
+
+
+
+
+
+// ts-node src/3/3-10.ts
